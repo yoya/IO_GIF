1a5621b04f2b1737857974b6d7a89e8d656e0387
Yoshihiro Yamazaki <yoya@awm.jp>
2020-02-18 01:58:00 +0900

    function dumpLZWCode_Giflib we can get clearcode only.

--- IO/GIF/LZW.php
+++ IO/GIF/LZW.php
@@ -68,4 +68,66 @@ class IO_GIF_LZW {
             }
         }
     }
+    /*
+     * from giflib
+     * https://sourceforge.net/p/giflib/code/
+     */
+    const LZ_MAX_CODE  = 4095;
+    const NO_SUCH_CODE = 4098;  /* Impossible code, to signal empty. */
+    const LZ_BITS      = 12;
+    var $_data;
+    var $_data_offset;
+    function DGifBufferedInput(&$NextByte) {
+        $NextByte = ord($this->_data{$this->_data_offset});
+        $this->_data_offset++;
+    }
+    function DGifSetupDecompress($codeBits) {
+        $this->BitPerPixel = $codeBits;
+        $this->ClearCode = (1 << $this->BitPerPixel);
+        $this->EOFCode = $this->ClearCode + 1;
+        $this->RunningCode = $this->EOFCode + 1;
+        $this->RunningBits = $this->BitPerPixel + 1;
+        $this->MaxCode1 = 1 << $this->RunningBits;
+        $this->StackPtr = 0;
+        $this->LastCode = null;
+        $this->CrntShiftState = 0;
+        $this->CrntShiftDWord = 0;
+        $this->Prefix = new SplFixedArray(self::LZ_MAX_CODE+1);
+        for ($i = 0; $i <= self::LZ_MAX_CODE; $i++) {
+            $this->Prefix[$i] = self::NO_SUCH_CODE;
+        }
+    }
+    function DGifDecompressInput(&$Code) {
+        static $CodeMasks = [ 0x0000, 0x0001, 0x0003, 0x0007,
+                              0x000f, 0x001f, 0x003f, 0x007f,
+                              0x00ff, 0x01ff, 0x03ff, 0x07ff,
+                              0x0fff ];
+        if ($this->RunningBits > self::LZ_BITS) {
+            throw new Exception("D_GIF_ERR_IMAGE_DEFECT");
+        }
+        $NextByte = null;
+        while ($this->CrntShiftState < $this->RunningBits) {
+            $this->DGifBufferedInput($NextByte);
+            $this->CrntShiftDWord |= $NextByte << $this->CrntShiftState;
+            $this->CrntShiftState += 8;
+        }
+        $Code = $this->CrntShiftDWord & $CodeMasks[$this->RunningBits];
+        $this->CrntShiftDWord >>= $this->RunningBits;
+        $this->CrntShiftState -= $this->RunningBits;
+        if ($this->RunningCode < self::LZ_MAX_CODE + 2 &&
+            ++$this->RunningCode > $this->MaxCode1 &&
+            $this->RunningBits < self::LZ_BITS) {
+            $this->MaxCode1 <<= 1;
+            $this->RunningBits++;
+        }
+    }
+    function dumpLZWCode_Giflib($LZWcode, $codeBits, $indicesSize) {
+        // DGifBufferedInput initialize
+        $this->_data = $LZWcode;
+        $this->_data_offset = 0;
+        $this->DGifSetupDecompress($codeBits);
+        $Code = null;
+        $this->DGifDecompressInput($Code);
+        var_dump($Code);
+    }
 }
